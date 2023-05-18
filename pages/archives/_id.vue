ff0e6532ad36117f1a849bd32d14d21ffef738e9
cony-design <takeo.kotani@gmail.com>
2023-05-18 14:57:56 +0900

    プロジェクト一式コミット

---
+++ pages/archives/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>{{ post.title.rendered }}</h2>
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <div v-html="post.content.rendered"></div>
+    <nuxt-link to="/">トップページへ</nuxt-link>
+  </div>
+</template>
+
+<script>
+	export default {
+    async asyncData(context) {
+      const post = await context.$axios.$get(`http://blog.cony-design.com/wp-json/wp/v2/posts/${context.params.id}`)
+      return { post }
+    }
+	};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        日付: {{ post.date }}<br>
+        <NuxtLink :to="'/archives/'+post.id">title: {{ post.title.rendered }}</NuxtLink>
+        {{ post.excerpt.rendered }}<br>
+      </li>
+    </ul>
+	</div>
+</template>
+
+<script>
+	export default {
+    async asyncData({$axios}) {
+      const posts = await $axios.$get('http://blog.cony-design.com/wp-json/wp/v2/posts')
+      return { posts }
+    }
+	};
+</script>
